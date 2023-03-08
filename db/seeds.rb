ae6090eb05ab213e5fff18da09189562d4c6f937
Suzanne Goncalves <suzanne.g.p@hotmail.fr>
2023-03-08 16:32:19 +0100

    Add container on Devise and modify home page whith the card and the gradient

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,8 +14,8 @@ User.destroy_all
 user1 = User.create(email: "toto@gmail.com", password:"123456", username: "toto")
 
 
-paul_cluzot = Candidat.create(photo: File.open(Rails.root.join('app', 'assets', 'images', 'centre.jpg')), first_name: "Paul", last_name:"Cluzot", partie:"liberal")
-marie_clotilde_autain = Candidat.create(photo: File.open(Rails.root.join('app', 'assets', 'images', 'droite.png')), first_name: "Marie-Clotilde", last_name:"Autain", partie:"l'array publique")
-maryame_taubica = Candidat.create(photo: File.open(Rails.root.join('app', 'assets', 'images', 'extreme-gauche.png')), first_name: "maryame", last_name:"taubica", partie:"la france indomptable")
-rabbia_kelaoua = Candidat.create(photo: File.open(Rails.root.join('app', 'assets', 'images', 'gauche.png')), first_name: "rabbia", last_name:"kelaoua", partie:"la democratie en marche")
-gerard_martin = Candidat.create(photo: File.open(Rails.root.join('app', 'assets', 'images', 'extreme-droite.png')), first_name: "Gerard", last_name:"Martin", partie:"partie nationaliste")
+paul_cluzot = Candidat.create(photo: 'centre.jpg', first_name: "Paul", last_name:"Cluzot", partie:"Liberal")
+marie_clotilde_autain = Candidat.create(photo: 'droite.png', first_name: "Marie-Clotilde", last_name:"Autain", partie:"L'array publique")
+maryame_taubica = Candidat.create(photo: 'extreme-gauche.png', first_name: "Maryame", last_name:"Taubica", partie:"La france indomptable")
+rabbia_kelaoua = Candidat.create(photo: 'gauche.png', first_name: "Rabbia", last_name:"Kelaoua", partie:"La democratie en marche")
+gerard_martin = Candidat.create(photo: 'extreme-droite.png', first_name: "Gerard", last_name:"Martin", partie:"Partie nationaliste")
