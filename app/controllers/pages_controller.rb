3fc7d57e9bb802aaaa79b93aee7f2018b8d9a5a8
Suzanne Goncalves <suzanne.g.p@hotmail.fr>
2023-03-13 11:12:48 +0100

    just to pull

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
   def home
     @candidats = Candidat.all
+    @winner = @candidats.sort_by{ |candidat| candidat.compteur}.last
   end
 end
