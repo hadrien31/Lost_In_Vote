f7563cd5d40e406852eb98fd62dbc40fe2165c1e
hadrien mari <hadrienmari@yahoo.fr>
2023-03-15 12:19:53 +0100

    methode pour le top 3 test

--- app/models/candidat.rb
+++ app/models/candidat.rb
@@ -18,4 +18,35 @@ class Candidat < ApplicationRecord
     # on recupère le candidat correspondant
     return scores_sorted.last.candidat
   end
+
+  def self.second_for(user)
+    return nil unless user
+
+    # On recuper le score de chaque candidat pour ce user
+    scores = Candidat.all.map do |candidat|
+      Score.find_by(candidat: candidat, user: user)
+    end.compact
+    return nil if scores.length < 5
+
+    # ou prend le plus haut score
+    scores_sorted = scores.sort_by(&:value)
+    # on recupère le candidat correspondant
+    return scores_sorted[3].candidat
+  end
+
+  def self.third_for(user)
+    return nil unless user
+
+    # On recuper le score de chaque candidat pour ce user
+    scores = Candidat.all.map do |candidat|
+      Score.find_by(candidat: candidat, user: user)
+    end.compact
+    return nil if scores.length < 5
+
+    # ou prend le plus haut score
+    scores_sorted = scores.sort_by(&:value)
+    # on recupère le candidat correspondant
+    return scores_sorted[2].candidat
+  end
+
 end
